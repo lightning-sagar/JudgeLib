048ba362e06ed0909a2f058b7870d03058a50be4
Sagar Seth <lightningsagar0@gmail.com>
2025-06-12 23:23:13 +0530

    updated the path of cron

--- src/api/cron/route.ts
+++ src/api/cron/route.ts
@@ -10,11 +10,6 @@ const links = [
 ];
 
 export async function GET(request: Request) {
-  // Optional: Add secret check
-  const authHeader = request.headers.get("Authorization");
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return new Response('Unauthorized', { status: 401 });
-  }
 
   const results = await Promise.all(
     links.map(async (url) => {
